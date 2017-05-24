0ef24c295b10d166241e4dd9c0723c8e7109cb79
Hausdorffcode <m15950172843@163.com>
2017-05-24 14:42:24 +0800

    version1

---
+++ ChatDemo/ChatDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        private const int BufferSize = 1024;
+
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[BufferSize];
+            IPEndPoint iped = null;
+
+            while (true)
+            {
+                iped = MyNetworkLibrary.AddressHelper.GetRemoteMachineIPEndPoint();
+                if (iped != null)
+                    break;
+            }
+
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                client.Connect(iped);
+                IPAddress localIp = MyNetworkLibrary.AddressHelper.GetLocalhostIPv4Addresses().First();
+                string msg = string.Format("客户端{0}发来贺电。", localIp);
+                
+                client.Send(Encoding.UTF8.GetBytes(msg));
+                client.Shutdown(SocketShutdown.Both);
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("无法连接到主机：{0}，原因：{1}\nNativeErrorCode: \nSocketErrorCode: ", iped.Address, e.Message, e.NativeErrorCode, e.SocketErrorCode);
+            }
+            finally
+            {
+                client.Close();
+            }
+            Console.WriteLine("消息发送完毕，断开与服务端的连接。");
+            Console.WriteLine("敲任意键退出...");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ ChatDemo/Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Program
+    {
+        private const int BufferSize = 1024;
+
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = MyNetworkLibrary.AddressHelper.GetLocalhostIPv4Addresses().First();
+            int endpoint = MyNetworkLibrary.AddressHelper.GetOneAvailablePortInLocalhost();
+            IPEndPoint ipep = new IPEndPoint(ipAddress, endpoint);
+
+            byte[] data = new byte[BufferSize];
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            using (server)
+            {
+                server.Bind(ipep);
+                server.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine("主机{0}正在监听端口{1}...", ipAddress, endpoint);
+                    Socket client = server.Accept();
+                    IPEndPoint cipep = (IPEndPoint)client.RemoteEndPoint;
+                    Console.WriteLine("已经接受来自ip为{0}，端口为{1}的客户连接。", cipep.Address, cipep.Port);
+                    int recv = client.Receive(data);
+                    Console.WriteLine(Encoding.UTF8.GetString(data, 0, recv));
+                    Console.WriteLine("关闭请求");
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+        }
+    }
+}
